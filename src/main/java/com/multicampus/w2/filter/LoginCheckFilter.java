fe1256ed7acaebe9d27728ccba20a5b2b27b961c
thatzcool <mycaptain0771@gmail.com>
2023-03-14 13:03:00 +0900

    filter_ok

--- src/main/java/com/multicampus/w2/filter/LoginCheckFilter.java
+++ src/main/java/com/multicampus/w2/filter/LoginCheckFilter.java
@@ -1,6 +1,10 @@
-package com.multicampus.w2.filter;
+package org.zerock.w2.filter;
 
+import com.multicampus.w2.dto.MemberDTO;
+import com.multicampus.w2.service.MemberService;
 import lombok.extern.log4j.Log4j2;
+import com.multicampus.w2.dto.MemberDTO;
+import com.multicampus.w2.service.MemberService;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -12,71 +16,90 @@ import java.io.IOException;
 import java.util.Arrays;
 import java.util.Optional;
 
-import com.multicampus.w2.dto.MemberDTO;
-import com.multicampus.w2.service.MemberService;
+
 @WebFilter(urlPatterns = {"/todo/*"})
 @Log4j2
 public class LoginCheckFilter implements Filter {
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//
+//        log.info("Login check filter....");
+//
+//        chain.doFilter(request, response);
+//    }
+//    @Override
+//    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+//
+//        log.info("Login check filter....");
+//
+//        HttpServletRequest req = (HttpServletRequest)request;
+//        HttpServletResponse resp = (HttpServletResponse)response;
+//
+//        HttpSession session = req.getSession();
+//
+//        if(session.getAttribute("loginInfo") == null){
+//
+//            resp.sendRedirect("/login");
+//
+//            return;
+//        }
+//
+//        chain.doFilter(request, response);
+//    }
+
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-         log.info("Login check filter...");
-        HttpServletRequest req = (HttpServletRequest) request;
-        HttpServletResponse resp = (HttpServletResponse) response;
+
+        log.info("Login check filter....");
+
+        HttpServletRequest req = (HttpServletRequest)request;
+        HttpServletResponse resp = (HttpServletResponse)response;
 
         HttpSession session = req.getSession();
-        if(session.getAttribute("loginInfo") != null){
-            chain.doFilter(request,response);
-            return;
-        }
 
-        //session에 loginInfo 값이 없다면
-        //쿠키를 체크
-        Cookie cookie = findCookie(((HttpServletRequest) request).getCookies(),"remember-me");
-        //세션에도 없고 쿠키에도 없다면 그냥 로그인으로
-        if(cookie == null) {
+        if(session.getAttribute("loginInfo") == null){
+
+            //쿠키를 체크
+            Cookie cookie = findCookie(req.getCookies(), "remember-me");
+
+            if(cookie != null){
+
+                log.info("cookie는 존재하는 상황");
+                String uuid  = cookie.getValue();
+
+                try {
+                    MemberDTO memberDTO = MemberService.INSTANCE.getByUUID(uuid);
+
+                    log.info("쿠키의 값으로 조회한 사용자 정보: " + memberDTO );
+
+                    session.setAttribute("loginInfo", memberDTO);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+
+                chain.doFilter(request, response);
+                return;
+            }
+
             resp.sendRedirect("/login");
+
             return;
         }
-        //쿠키가 존재하는 상황이라면
-        log.info("------cookie 존재하면--------");
-        String uuid = cookie.getValue();
 
-        try{  //데이터베이스 확인
-            //쿠기 uuid 값으로 사용자 정보 조회  ==> MemberDTO 저장
-             MemberDTO memberDTO = MemberService.INSTANCE.getByUUID(uuid);
-             if(memberDTO == null){
-                 throw  new Exception("cookie value is not valid");
-             }
-             session.setAttribute("loginInfo",memberDTO);
-             chain.doFilter(request,response);
-
-            //MemberDTO 를 세션에 저장 (추가)  ==>  TodoReadController
-
-        }catch (Exception e){
-            e.printStackTrace();
-            ((HttpServletResponse) response).sendRedirect("/login");
-
-        }
-
-
-
-
-
-
-
-
-
-
+        chain.doFilter(request, response);
     }
 
-    private Cookie findCookie(Cookie[] cookies, String name) {
-        if(cookies == null  || cookies.length == 0) {
+    private Cookie findCookie(Cookie[] cookies, String name){
+
+        if(cookies == null || cookies.length == 0){
             return null;
         }
 
         Optional<Cookie> result = Arrays.stream(cookies).filter(ck -> ck.getName().equals(name)).findFirst();
-         return result.isPresent()? result.get():null;
+
+        return result.isPresent()?result.get():null;
     }
+
+
 }
-//Filter 인터페이스를 import javax.servlet.Filter   : doFilter()필터링이 필요한 로직을 구현하는 메소드
-//@WebFilter 어너테이션 추가
--- src/main/java/com/multicampus/w2/filter/UTF8Filter.java
+++ src/main/java/com/multicampus/w2/filter/UTF8Filter.java
@@ -1,4 +1,4 @@
-package com.multicampus.w2.filter;
+package org.zerock.w2.filter;
 
 import lombok.extern.log4j.Log4j2;
 
@@ -10,11 +10,17 @@ import java.io.IOException;
 @WebFilter(urlPatterns = {"/*"})
 @Log4j2
 public class UTF8Filter implements Filter {
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        log.info("UTF - filter.....");
-        HttpServletRequest req = (HttpServletRequest) request;
+
+        log.info("UTF8  filter....");
+
+        HttpServletRequest req = (HttpServletRequest)request;
+
         req.setCharacterEncoding("UTF-8");
-        chain.doFilter(request,response);
+
+
+        chain.doFilter(request, response);
     }
 }
